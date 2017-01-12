e392739e55480c4b18edd50514cada18609b5fae
Charles Zhang <charleszhang99@yahoo.com>
2017-01-11 23:08:15 -0500

    Change to new instruction set and assembly language

--- compiler.lisp
+++ compiler.lisp
@@ -1,40 +1,12 @@
 ;; parse program
 
-(defun whitespace-char-p (char)
-  (member char '(#\Space #\Newline #\Tab) :test #'char=))
-
-(defun read-instruction (stream)
-  (let ((line-stream (make-string-input-stream (read-line stream)))
-        (no-second-arg (cons nil nil))
-        (no-third-arg (cons nil nil)))
-    (let ((name (read line-stream))
-          (arg (read line-stream nil no-second-arg))
-          (arg2 (read line-stream nil no-third-arg)))
-      (cond ((eq arg no-second-arg) (list name))
-            ((or (consp arg) (symbolp arg) (numberp arg))
-             (list name arg arg2))
-            (t (error "Unhandled second argument to ~a: ~a" name arg))))))
-
-(defun read-assembly-line (first-char stream)
-  (cond ((alpha-char-p first-char)
-         (read-instruction stream))
-        ((char= #\. first-char)
-         (read stream))
-        ((or (char= first-char #\;)
-             (whitespace-char-p first-char))
-         (read-line stream)
-         nil)
-        (t (error "Unhandled first character to line: ~a" first-char))))
-
 (defun read-assembly-file (pathname)
-  (let (program (eof '#:eof))
+  (let ((eof '#:eof) program)
     (with-open-file (stream pathname :direction :input)
-      (loop (let ((char (peek-char nil stream nil eof)))
-              (when (eq char eof)
+      (loop (let ((form (read stream nil eof)))
+              (when (eq form eof)
                 (return))
-              (let ((line (read-assembly-line char stream)))
-                (when line
-                  (push line program)))))
+              (push form program)))
       (nreverse program))))
 
 ;; SEGMENT - state of the assembler
@@ -59,7 +31,7 @@
 (defun segment-current-instruction-address (segment)
   (multiple-value-bind (q r)
       ;; FIXME magic number
-      (floor (fill-pointer (segment-buffer segment)) 2)
+      (floor (print (fill-pointer (print (segment-buffer segment)))) 2)
     (assert (zerop r))
     q))
 
@@ -71,16 +43,23 @@
     (gethash name instruction-emitters)))
 
 (defmacro define-bitfield-layout (name &rest byte-specs)
-  (let ((args (mapcar (lambda (byte-spec)
-                        (alexandria:symbolicate
-                         "BYTE-" (write-to-string (second byte-spec))
-                         "-" (write-to-string (third byte-spec))))
-                      byte-specs)))
-    `(defun ,name ,args
-       (let ((byte 0))
-         ,@(mapcar (lambda (byte-spec arg) `(setf (ldb ,byte-spec byte) ,arg))
-                   byte-specs args)
-         byte))))
+  (let (emitter-name args)
+    (dolist (byte-spec byte-specs)
+      (if (and (listp byte-spec) (eq (first byte-spec) :emitter))
+          (setf emitter-name (second byte-spec))
+          (push (alexandria:symbolicate
+                 "BYTE-" (write-to-string (second byte-spec))
+                 "-" (write-to-string (third byte-spec)))
+                args)))
+    (let ((args (nreverse args)))
+      `(progn
+         (defun ,name ,args
+           (let ((byte 0))
+             ,@(mapcar (lambda (byte-spec arg) `(setf (ldb ,byte-spec byte) ,arg))
+                       byte-specs args)
+             byte))
+         (defun ,emitter-name (segment ,@args)
+           (emit-byte segment (,name ,@args)))))))
 
 ;; more options to be defined here... more general than necessary for now
 (defmacro define-instruction (name lambda-list &rest options)
@@ -94,15 +73,7 @@
 
 ;; register
 (defparameter +register-encoding-map+
-  #(pc ; program counter (000)
-    sp ; stack counter   (001)
-    fp ; frame pointer   (010)
-    sr ; status register (011)
-    ax ; general purpose (100)
-    bx ; general purpose (101)
-    cx ; general purpose (110)
-    dx ; general purpose (111)
-    ))
+  #(%pc %sp %fp %ax %bx %cx %dx %ex))
 
 (deftype register-encoding () '(unsigned-byte 3))
 (defun registerp (register)
@@ -113,20 +84,17 @@
 (defun immediatep (x) (typep x 'immediate))
 
 (define-bitfield-layout byte-opcode-with-reg
-  (byte 5 3) (byte 3 0))
+    (byte 5 3) (byte 3 0)
+  (:emitter emit-opcode-with-reg))
 (define-bitfield-layout byte-reg
-  (byte 3 5))
+    (byte 3 5)
+  (:emitter emit-byte-reg))
+(define-bitfield-layout byte-reg-reg
+    (byte 3 5) (byte 3 2)
+  (:emitter emit-reg-reg))
 (define-bitfield-layout byte-opcode-with-reg-reg
-  (byte 2 6) (byte 3 3) (byte 3 0))
-
-(defun emit-opcode-with-reg (segment opcode register-encoding)
-  (emit-byte segment (byte-opcode-with-reg opcode register-encoding)))
-
-(defun emit-reg (segment register-encoding)
-  (emit-byte segment (byte-reg register-encoding)))
-
-(defun emit-opcode-with-reg-reg (segment opcode reg1 reg2)
-  (emit-byte segment (byte-opcode-with-reg-reg opcode reg1 reg2)))
+    (byte 2 6) (byte 3 3) (byte 3 0)
+  (:emitter emit-opcode-with-reg-reg))
 
 (defun emit-immediate (segment immediate)
   (check-type immediate immediate)
@@ -135,17 +103,16 @@
                          immediate)))
 
 (defun memory-reference-p (x)
-  (typep x '(cons (eql @) t)))
+  (and (listp x) (eq (first x) '@+)))
 
 (defun check-memory-reference-syntax (memory-reference)
-  (unless (and (memory-reference-p memory-reference)
-               (eq (first (second memory-reference)) '+))
+  (unless (memory-reference-p memory-reference)
     (error "Not valid memory-reference syntax")))
 
 (defun parse-memory-reference (memory-reference)
   (check-memory-reference-syntax memory-reference)
-  (values (find-if #'registerp (second memory-reference))
-          (find-if #'immediatep (second memory-reference))))
+  (values (find-if #'registerp (rest memory-reference))
+          (find-if #'immediatep (rest memory-reference))))
 
 ;; emit instruction to segment
 (defun emit-inst (segment instruction &rest args)
@@ -162,11 +129,39 @@
                 (:emitter
                  (emit-opcode-with-reg segment ,opcode (register-encoding dest))
                  (emit-reg segment (register-encoding src))))))
-  (define-immediate-to-reg mcl #b00010)
-  (define-immediate-to-reg mch #b00100)
-  (define-reg-to-reg nor #b01010)
-  (define-reg-to-reg jnz #b01100)
-  (define-reg-to-reg jlz #b01110))
+  (define-immediate-to-reg andl #b00001)
+  (define-immediate-to-reg andh #b00010)
+  (define-immediate-to-reg orl #b00011)
+  (define-immediate-to-reg orh #b00100)
+  (define-immediate-to-reg movl #b00101)
+  (define-immediate-to-reg movh #b00110)
+  (define-reg-to-reg not #b01000))
+
+(define-instruction brnz (segment reg absolute-address)
+  (:emitter
+   ;; we want to emit a relative address offset. calculate that now
+   ;; because emitting a byte will chagne the address (KLUDGE)
+   (let ((relative-offset (- absolute-address (segment-current-instruction-address segment))))
+     (emit-opcode-with-reg segment #b00111 (register-encoding reg))
+     (emit-immediate segment relative-offset))))
+
+(define-instruction add (segment dest arg1 &optional arg2)
+  (:emitter
+   (cond (arg2
+          (emit-opcode-with-reg segment #b01001 (register-encoding dest))
+          (emit-reg-reg segment (register-encoding arg1) (register-encoding arg2)))
+         (t
+          (emit-opcode-with-reg segment #b00000 (register-encoding dest))
+          (emit-immediate segment arg1)))))
+
+(macrolet ((def (name opcode)
+             `(define-instruction ,name (segment reg1 reg2 reg3)
+                (:emitter
+                 (emit-opcode-with-reg segment ,opcode (register-encoding reg1))
+                 (emit-reg-reg segment (register-encoding reg2)
+                               (register-encoding reg3))))))
+  (def and #b01010)
+  (def or #b01011))
 
 (define-instruction mov (segment dest src)
   (:emitter
@@ -189,24 +184,18 @@
                                       (register-encoding dest)
                                       (register-encoding reg))
             (emit-immediate segment offset)))
-         (t (emit-inst segment 'mcl dest (ldb (byte 8 0) src))
-            (emit-inst segment 'mch dest (ldb (byte 8 8) src))))))
+         (t (emit-inst segment 'movl dest (ldb (byte 8 0) src))
+            (emit-inst segment 'movh dest (ldb (byte 8 8) src))))))
 
-(define-instruction adc (segment dest src)
+(define-instruction push (segment reg)
   (:emitter
-   (cond ((registerp src)
-          (emit-opcode-with-reg segment #b01000 (register-encoding dest))
-          (emit-reg segment (register-encoding src)))
-         (t
-          (emit-opcode-with-reg segment #b00110 (register-encoding dest))
-          (emit-immediate segment src)))))
+   (emit-inst 'mov segment '(@+ %sp 0) reg)
+   (emit-inst 'add segment '%sp -1)))
 
 ;; assembler
 ;; labels support
-;; labels will be symbols
 (defun labelp (x)
-  (and (symbolp x)
-       (char= (char (symbol-name x) 0) #\.)))
+  (and (symbolp x) (not (registerp x))))
 
 (defun emit-instructions (segment instructions compute-labels-p)
   (flet ((resolve-label (arg)
@@ -216,13 +205,16 @@
                    (gethash arg (segment-label-map segment)))
                arg)))
     (dolist (instruction instructions)
-      (if (labelp instruction)
-          (when compute-labels-p
-            (emit-label segment instruction))
-          (destructuring-bind (op &rest args)
-              instruction
-            (apply #'emit-inst segment op
-                   (mapcar #'resolve-label args)))))))
+      (case (first instruction)
+        (.label (emit-label segment (second instruction)))
+        (.print (print (second instruction)))
+        (otherwise
+         (when compute-labels-p
+           (emit-label segment (second instruction)))
+         (destructuring-bind (op &rest args)
+             instruction
+           (apply #'emit-inst segment op
+                  (mapcar #'resolve-label args))))))))
 
 (defun assemble (segment instructions)
   ;; compute labels first, but copy the buffer to a new segment
@@ -238,16 +230,13 @@
   (with-open-file (stream binary-file :direction :output
                                       :element-type '(unsigned-byte 8)
                                       :if-exists :supersede)
-    (write-sequence (segment-buffer (assemble (make-segment)
-                                      instructions))
+    (write-sequence (segment-buffer (assemble (make-segment) instructions))
                     stream)))
 
 (defun assemble-file-mif (instructions mif-file)
   (with-open-file (stream mif-file :direction :output
                                    :if-exists :supersede)
-    (let ((buffer (segment-buffer (assemble (make-segment)
-                                    instructions)))
-          (*print-base* 2))
+    (let ((buffer (segment-buffer (assemble (make-segment) instructions))))
       (loop for i from 0 to (fill-pointer buffer) by 2
             do (format stream "~8,'0b" (aref buffer i))
                (format stream "~8,'0b~%" (aref buffer (1+ i))))
